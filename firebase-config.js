829467ac85a9a76fada5afbefcd0cbf870b80f62
ForgeImpact <forge.impact09@gmail.com>
2025-04-03 16:18:59 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -9,4 +9,4 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_
+  messagingSenderId: process.env.REACT_APP_}
